4d73e3f445a97e763e81159038e95d6b1ab4dc39
Liliacea <Liliacea_85@mail.ru>
2023-03-08 21:36:51 +0600

    tests passed

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -4,42 +4,25 @@ public class Converter {
     private Iterator<Integer> currentIterator;
     private Iterator<Iterator<Integer>> iterator;
 
-
-    public Iterator<Integer> convert(Iterator<Iterator<Integer>>... iterators) {
-        this.iterator = iterator;
-      //  this.currentIterator =
-
-
+    public Converter(Iterator<Iterator<Integer>> iterator) {
+    this.iterator = iterator;
+    }
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> iterator) {
         return new Iterator<Integer>() {
-
-
-
-            {
-
-                List<Iterator<Integer>> iteratorList = new ArrayList<>(iterators.length); //список итераторов
-                Collections.addAll(iteratorList); //коллекция на основе
-                iterator = iteratorList.iterator(); //итератор = итератор вот этого самого списка, который уже коллекция
-
-            }
-
             private void check() {
                 if (currentIterator == null && iterator.hasNext())
                     currentIterator = iterator.next();
             }
 
-
             @Override
             public boolean hasNext() {
-
                 check();
                 if (currentIterator == null)
                     return false;
                 if (currentIterator.hasNext())
                     return true;
-
                 if (iterator.hasNext())
                     currentIterator = iterator.next();
-
                 return currentIterator.hasNext();
             }
 
@@ -48,10 +31,8 @@ public class Converter {
                 check();
                 if (currentIterator == null)
                     throw new NoSuchElementException();
-
                 if (!currentIterator.hasNext() && iterator.hasNext())
                     currentIterator = iterator.next();
-
                 return currentIterator.next();
             }
         };
