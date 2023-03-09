5e1df57dd209bff7fbdc7f45ca7b6a21b32071b7
Liliacea <Liliacea_85@mail.ru>
2023-03-09 11:25:15 +0600

    function check was changed

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 public class Converter {
     private Iterator<Integer> currentIterator;
-    private Iterator<Iterator<Integer>> iterator;
 
-    public Converter(Iterator<Iterator<Integer>> iterator) {
-    this.iterator = iterator;
-    }
     public Iterator<Integer> convert(Iterator<Iterator<Integer>> iterator) {
         return new Iterator<Integer>() {
             private void check() {
+
                 if (currentIterator == null && iterator.hasNext())
+                    currentIterator = iterator.next();
+                if (!currentIterator.hasNext() && iterator.hasNext())
                     currentIterator = iterator.next();
             }
 
@@ -29,10 +28,9 @@ public class Converter {
             @Override
             public Integer next() {
                 check();
-                if (currentIterator == null)
+                if (currentIterator == null) {
                     throw new NoSuchElementException();
-                if (!currentIterator.hasNext() && iterator.hasNext())
-                    currentIterator = iterator.next();
+                }
                 return currentIterator.next();
             }
         };
--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,7 +29,7 @@ public class ConverterTest {
         Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
         Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        Converter iteratorOfIterators = new Converter(its);
+        Converter iteratorOfIterators = new Converter();
         it = iteratorOfIterators.convert(its);
 
     }
@@ -104,7 +105,7 @@ public class ConverterTest {
         Iterator<Integer> it2 = (new ArrayList<Integer>()).iterator();
         Iterator<Integer> it3 = (new ArrayList<Integer>()).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        Converter iteratorOfIterators = new Converter(its);
+        Converter iteratorOfIterators = new Converter();
         it = iteratorOfIterators.convert(its);
         assertThat(it.hasNext(), is(false));
     }
@@ -116,12 +117,27 @@ public class ConverterTest {
     public void invocationOfNextMethodShouldThrowNoSuchElementException() {
         Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1).iterator();
-        Converter iteratorOfIterators = new Converter(its);
+        Converter iteratorOfIterators = new Converter();
         it = iteratorOfIterators.convert(its);
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(3));
         it.next();
+    }
+
+    @Test
+    public void hasNextShouldReturnTheSameValuesInCaseOfMultiplyInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void currentIteratorNotNull() {
+   // assertNull();
     }
 
 }
