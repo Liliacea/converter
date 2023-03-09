bd9162ae85d166795669f87cbc63d536fc9d4a8b
Liliacea <Liliacea_85@mail.ru>
2023-03-09 12:02:39 +0600

    test moreIterators was added

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -157,5 +157,29 @@ public class ConverterTest {
 
         assertNotNull(it1);
     }
+    @Test ()
+    public void moreIterators() {
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
+        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
+        Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
+        Iterator<Integer> it4 = Arrays.asList(10,11,12).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3, it4).iterator();
+        Converter iteratorOfIterators = new Converter();
+        it = iteratorOfIterators.convert(its);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+        assertThat(it.next(), is(7));
+        assertThat(it.next(), is(8));
+        assertThat(it.next(), is(9));
+        assertThat(it.next(), is(10));
+        assertThat(it.next(),is(11));
+        assertThat(it.next(), is(12));
+    }
 
 }
